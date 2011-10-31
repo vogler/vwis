fa90d6596d1142a99690a8632d69ea616bb3ab67
Sebastian Wiesner <lunaryorn@googlemail.com>
2011-10-31 19:37:57 +0100

    Turned iterator into interface deriving from AutoCloseable and added documentation

--- 2/VWIS_2/src/DBIterator.java
+++ 2/VWIS_2/src/DBIterator.java
@@ -1,9 +1,25 @@
 
-public abstract class DBIterator {
-	
-	abstract String[] open() throws Exception;
+/**
+ * Iterator over a database relation.
+ */
+public interface DBIterator extends AutoCloseable {
 
-	abstract Object[] next() throws Exception;
+    /**
+     * Opens the relation.
+     * 
+     * Re-opening a relation which was already opened resets the iterator.  The
+     * next call to next() will return the first tuple again.
+     * 
+     * @return the names of the attributes of the relation
+     * @throws Exception if opening fails
+     */
+    public String[] open() throws Exception;
 
-	abstract void close() throws Exception;
+    /**
+     * Gets the next tuple from the relation.
+     * 
+     * @return the next tuple, or null, if no further tuples exist in this relation
+     * @throws Exception if the tuple could not be retrieved
+     */
+    public Object[] next() throws Exception;
 }
