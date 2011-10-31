ed3fb5afbd545c4ee80999ee9522e4e5ed18ae39
Sebastian Wiesner <lunaryorn@googlemail.com>
2011-10-31 20:44:42 +0100

    Improved test

--- 2/VWIS_2/src/Test.java
+++ 2/VWIS_2/src/Test.java
@@ -1,45 +1,40 @@
+
 import java.util.Arrays;
 
-
 public class Test {
+    
+    private static void printRelation(final DBIterator relation) 
+            throws Exception {
+        final String[] headers = relation.open();
+        System.out.println(Arrays.toString(headers));
+        Object[] tuple;
+        while ((tuple = relation.next()) != null) {
+            System.out.println(Arrays.toString(tuple));
+        }
+    }
 
-	/**
-	 * @param args
-	 * @throws Exception 
-	 */
-	public static void main(String[] args) throws Exception {
-		Tablescan rel_a = new Tablescan("rel_a.txt");
-		Tablescan rel_b = new Tablescan("rel_b.txt");
-		
-		System.out.println("--- Relation A ---");
-		String[] headers_a = rel_a.open();
-		System.out.println("Attributes: "+Arrays.toString(headers_a));
-		Object[] left;
-		while((left = rel_a.next()) != null){
-			System.out.println(Arrays.toString(left));
-		}
-		
-		System.out.println();
-		
-		System.out.println("--- Relation B ---");
-		String[] headers_b = rel_b.open();
-		System.out.println("Attributes: "+Arrays.toString(headers_b));
-		Object[] right;
-		while((right = rel_b.next()) != null){
-			System.out.println(Arrays.toString(right));
-		}
-		
-		System.out.println();
-		
-		NLJoin nljoin = new NLJoin(rel_a, rel_b);
-		System.out.println("--- NLJoin A B ---");
-		String[] headers_j = nljoin.open();
-		System.out.println("Attributes: "+Arrays.toString(headers_j));
-		Object[] join;
-		while((join = nljoin.next()) != null){
-			System.out.println(Arrays.toString(join));
-		}
-		
-	}
-
+    public static void main(String[] args) throws Exception {
+        final Tablescan A = new Tablescan("rel_a.txt");
+        final Tablescan B = new Tablescan("rel_b.txt");
+        
+        try (final Tablescan r = A) {
+            System.out.println("--- Relation A ---");
+            printRelation(r);
+        }
+        
+        System.out.println();
+        
+        try (final Tablescan r = B) {
+            System.out.println("--- Relation B ---");
+            printRelation(r);
+        }
+             
+        System.out.println();
+        
+        try (final NLJoin joined = new NLJoin(A, B)) {
+            System.out.println("--- A ⋈ B ---");
+            printRelation(joined);
+        }
+        
+    }
 }
