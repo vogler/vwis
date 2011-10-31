a9fda87ba5df93030346c12a84ca439b81dd6401
Sebastian Wiesner <lunaryorn@googlemail.com>
2011-10-31 22:28:53 +0100

    Implemented test client and test server

--- 2/VWIS_2/src/Test.java
+++ 2/VWIS_2/src/Test.java
@@ -1,17 +1,5 @@
 
-import java.util.Arrays;
-
 public class Test {
-    
-    private static void printRelation(final DBIterator relation) 
-            throws Exception {
-        final String[] headers = relation.open();
-        System.out.println(Arrays.toString(headers));
-        Object[] tuple;
-        while ((tuple = relation.next()) != null) {
-            System.out.println(Arrays.toString(tuple));
-        }
-    }
 
     public static void main(String[] args) throws Exception {
         final Tablescan A = new Tablescan("rel_a.txt");
@@ -19,21 +7,21 @@ public class Test {
         
         try (final Tablescan r = A) {
             System.out.println("--- Relation A ---");
-            printRelation(r);
+            Util.printRelation(r);
         }
         
         System.out.println();
         
         try (final Tablescan r = B) {
             System.out.println("--- Relation B ---");
-            printRelation(r);
+            Util.printRelation(r);
         }
              
         System.out.println();
         
         try (final NLJoin joined = new NLJoin(A, B)) {
             System.out.println("--- A ⋈ B ---");
-            printRelation(joined);
+            Util.printRelation(joined);
         }
         
     }
