e7b7a90173def4f143852d32d7196f219a6902bb
Sebastian Wiesner <lunaryorn@googlemail.com>
2011-11-03 22:59:28 +0100

    Unit test for Tablescan

---
+++ 3/src/test/java/de/tum/in/vwis/group14/db/TablescanTest.java
@@ -0,0 +1,128 @@
+package de.tum.in.vwis.group14.db;
+
+import java.io.InputStream;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.List;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.nio.file.FileSystems;
+import org.junit.Rule;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import static org.junit.Assert.*;
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
+
+/**
+ * Testcase for tablescan
+ */
+public class TablescanTest {
+
+    @Rule
+    public TemporaryFolder testFolder = new TemporaryFolder();
+
+    public TablescanTest() {
+    }
+
+    @Before
+    public void setUp() throws IOException {
+        final URL resource = this.getClass().getResource("/test_relation");
+        this.relationFileName = FileSystems.getDefault().getPath(
+                this.testFolder.getRoot().getPath(), "test_relation");
+        try (final InputStream source = resource.openStream()) {
+            Files.copy(source, this.relationFileName);
+        }
+        this.relation = new Tablescan(this.relationFileName);
+
+    }
+
+    @Test
+    public void testOpen() throws Exception {
+        assertArrayEquals(NAMES, this.relation.open());
+    }
+
+    @Test
+    public void testOpenReopen() throws Exception {
+        this.relation.open();
+        assertArrayEquals(NAMES, this.relation.open());
+    }
+
+    @Test(expected = TableFormatException.class)
+    public void openEmptyFile() throws Exception {
+        final Tablescan empty = new Tablescan(
+                this.testFolder.newFile("empty_file").getPath());
+        empty.open();
+    }
+
+    @Test
+    public void testEmptyRelation() throws Exception {
+        final Charset utf8 = Charset.forName("UTF-8");
+        final List<String> lines = Files.readAllLines(
+                this.relationFileName, utf8);
+        final Path filename = FileSystems.getDefault().getPath(
+                this.testFolder.getRoot().getPath(), "empty_relation");
+        try (final BufferedWriter sink =
+                        Files.newBufferedWriter(filename, utf8)) {
+            sink.write(lines.get(0));
+            sink.write("\n");
+            sink.write(lines.get(1));
+            sink.write("\n");
+        }
+        final Tablescan empty = new Tablescan(filename);
+        assertArrayEquals(empty.open(), NAMES);
+        assertNull(empty.next());
+    }
+
+    @Test
+    public void testNext() throws Exception {
+        this.relation.open();
+        assertArrayEquals(FIRST_ROW, this.relation.next());
+        assertArrayEquals(SECOND_ROW, this.relation.next());
+        assertNull(this.relation.next());
+    }
+
+    @Test(expected = IOException.class)
+    public void testNextBeforeOpen() throws Exception {
+        this.relation.next();
+    }
+
+    @Test
+    public void testNextReopen() throws Exception {
+        this.relation.open();
+        assertArrayEquals(FIRST_ROW, this.relation.next());
+        assertArrayEquals(SECOND_ROW, this.relation.next());
+        this.relation.open();
+        assertArrayEquals(FIRST_ROW, this.relation.next());
+    }
+
+    @Test
+    public void testTypes() throws Exception {
+        this.relation.open();
+        final Object[] first = this.relation.next();
+        assertThat(first[0], instanceOf(Integer.class));
+        assertThat(first[1], instanceOf(String.class));
+        assertThat(first[2], instanceOf(Float.class));
+        assertThat(first[3], instanceOf(Double.class));
+    }
+
+    @Test
+    public void testClose() throws Exception {
+        this.relation.close();
+        try {
+            this.relation.next();
+            fail("next after close succeeded");
+        } catch (IOException error) {
+        }
+    }
+    private Path relationFileName;
+    private Tablescan relation;
+    private final static String[] NAMES =
+            new String[]{"intcol", "stringcol", "floatcol", "doublecol"};
+    private final static Object[] FIRST_ROW = new Object[]{
+        1, "foo", 1.55f, 10e6d};
+    private final static Object[] SECOND_ROW = new Object[]{
+        2, "bar", 2.55f, .5d};
+}
