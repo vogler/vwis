44801b8421a3d7bdee48b6b30789fc0621a6c074
Sebastian Wiesner <lunaryorn@googlemail.com>
2011-11-04 18:28:21 +0100

    Added in-memory iterator for testing

---
+++ 3/src/main/java/de/tum/in/vwis/group14/db/ListDBIterator.java
@@ -0,0 +1,88 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.tum.in.vwis.group14.db;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * A DBIterator interfacing a list.
+ */
+public class ListDBIterator implements DBIterator {
+
+    /**
+     * Creates a new iterator for the specified list.
+     * 
+     * @param names the attribute names
+     * @param tuples the tuples to iterate over
+     */
+    public ListDBIterator(String[] names, List<Object[]> tuples) {
+        if (names == null) {
+            throw new IllegalArgumentException("names null");
+        } else if (names.length < 1) {
+            throw new IllegalArgumentException("names empty");
+        }
+        if (tuples == null) {
+            throw new IllegalArgumentException("tuples null");
+        }
+        this.names = names;
+        this.tuples = tuples;
+        this.current = -1;
+    }
+
+    /**
+     * Opens the relation.
+     * 
+     * Resets the reference to the current row to the first row.  A subsequent 
+     * invocation of next() consequently returns the first tuple of the 
+     * relation.
+     * 
+     * @return the attribute names of this relation
+     */
+    @Override
+    public String[] open() {
+        this.current = 0;
+        return this.names;
+    }
+
+    /**
+     * Gets the next tuple from this relation.
+     * 
+     * @return the next tuple, or null, if there are no further tuples
+     * @throws IOException if the relation is closed
+     */
+    @Override
+    public Object[] next() throws IOException {
+        if (this.current < 0) {
+            throw new IOException("Relation closed");
+        } else if (this.current >= this.tuples.size()) {
+            return null;
+        } else {
+            final Object[] tuple = this.tuples.get(this.current);
+            this.current++;
+            return tuple;
+        }
+    }
+
+    /**
+     * Closes this relation.
+     */
+    @Override
+    public void close() {
+        this.current = -1;
+    }
+    /**
+     * The attribute names of this relation
+     */
+    private final String[] names;
+    /**
+     * The tuples interfaced by this iterator
+     */
+    private final List<Object[]> tuples;
+    /**
+     * The current row
+     */
+    private int current;
+}
---
+++ 3/src/test/java/de/tum/in/vwis/group14/db/ListDBIteratorTest.java
@@ -0,0 +1,85 @@
+package de.tum.in.vwis.group14.db;
+
+import com.sun.jmx.remote.internal.ArrayQueue;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
+/**
+ *
+ */
+public class ListDBIteratorTest {
+
+    private static final String[] NAMES = new String[]{"id", "name"};
+    private static final List<Object[]> TUPLES;
+    private ListDBIterator relation;
+
+    static {
+        TUPLES = new ArrayList<>();
+        TUPLES.add(new Object[]{1, "bar"});
+        TUPLES.add(new Object[]{2, "foo"});
+    }
+
+    public ListDBIteratorTest() {
+    }
+
+    @Before
+    public void setUp() {
+        this.relation = new ListDBIterator(NAMES, TUPLES);
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void testNullNames() {
+        new ListDBIterator(null, TUPLES);
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void testEmptyNames() {
+        new ListDBIterator(new String[] {}, TUPLES);
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void testNullTuples() {
+        new ListDBIterator(NAMES, null);
+    }
+    
+    @Test
+    public void testEmptyRelation() throws IOException {
+        this.relation = new ListDBIterator(
+                NAMES, new ArrayList<Object[]>());
+        assertArrayEquals(NAMES, this.relation.open());
+        assertNull(this.relation.next());
+    }
+    
+    @Test
+    public void testOpen() {
+        assertArrayEquals(NAMES, this.relation.open());
+        // test re-opening
+        assertArrayEquals(NAMES, this.relation.open());
+    }
+
+    @Test
+    public void testNext() throws IOException {
+        this.relation.open();
+        assertArrayEquals(TUPLES.get(0), this.relation.next());
+        assertArrayEquals(TUPLES.get(1), this.relation.next());
+        assertNull(this.relation.next());
+    }
+    
+    @Test(expected=IOException.class)
+    public void testNextBeforeOpen() throws IOException {
+        this.relation.next();
+    }
+
+    @Test
+    public void testClose() {
+        this.relation.close();
+        try {
+            this.relation.next();
+            fail("Operation on closed relation succeeded");
+        } catch (IOException error) {}
+    }
+}
