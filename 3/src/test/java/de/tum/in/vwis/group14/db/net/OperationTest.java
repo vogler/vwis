982e3de13c16a6471e895acd34b7146ed56ebe73
Sebastian Wiesner <lunaryorn@googlemail.com>
2011-11-05 16:05:52 +0100

    Unittest for Operation

---
+++ 3/src/test/java/de/tum/in/vwis/group14/db/net/OperationTest.java
@@ -0,0 +1,84 @@
+package de.tum.in.vwis.group14.db.net;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+import java.util.ArrayList;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author lunar
+ */
+public class OperationTest {
+
+    private static final byte[] ORDINALS;
+
+    static {
+        ORDINALS = new byte[Operation.values().length];
+        for (int i = 0; i < ORDINALS.length; ++i) {
+            ORDINALS[i] = (byte) Operation.values()[i].ordinal();
+        }
+    }
+
+    public OperationTest() {
+    }
+
+    @Test
+    public void testWriteTo() throws IOException {
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+        for (final Operation op : Operation.values()) {
+            op.writeTo(out);
+        }
+        assertArrayEquals(ORDINALS, out.toByteArray());
+    }
+
+    @Test
+    public void testReadFrom() throws IOException, UnknownOperationException {
+        final ByteArrayInputStream in = new ByteArrayInputStream(ORDINALS);
+        final List<Operation> readOperations = new ArrayList<>();
+        Operation op;
+        while ((op = Operation.readFrom(in)) != null) {
+            readOperations.add(op);
+        }
+        assertArrayEquals(Operation.values(), readOperations.toArray());
+    }
+
+    @Test(expected = UnknownOperationException.class)
+    public void testReadFromUnknown() throws IOException,
+            UnknownOperationException {
+        final Operation[] ops = Operation.values();
+        final byte unknownOrdinal = (byte) (ops[ops.length - 1].ordinal() + 10);
+        final ByteArrayInputStream in = new ByteArrayInputStream(
+                new byte[]{unknownOrdinal});
+        Operation.readFrom(in);
+    }
+
+    @Test
+    public void testReadEndOfStream() throws IOException,
+            UnknownOperationException {
+        final ByteArrayInputStream in = new ByteArrayInputStream(new byte[]{});
+        assertNull(Operation.readFrom(in));
+    }
+
+    @Test
+    public void testWriteReadRoundtrip() throws IOException,
+            UnknownOperationException {
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+        for (final Operation op : Operation.values()) {
+            op.writeTo(out);
+        }
+        final ByteArrayInputStream in = new ByteArrayInputStream(
+                out.toByteArray());
+        final List<Operation> readOperations = new ArrayList<>();
+        Operation op;
+        while ((op = Operation.readFrom(in)) != null) {
+            readOperations.add(op);
+        }
+        assertArrayEquals(Operation.values(), readOperations.toArray());
+    }
+}
