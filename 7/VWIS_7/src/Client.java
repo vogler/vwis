779c4afee264118587b2c3c7834b0cde8a4f841b
Ralf Vogler <ralf.vogler@gmail.com>
2011-12-05 23:52:26 +0100

    Aufgabe 7

---
+++ 7/VWIS_7/src/Client.java
@@ -0,0 +1,129 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.transaction.xa.XAException;
+import javax.transaction.xa.XAResource;
+import javax.transaction.xa.Xid;
+
+import org.apache.derby.jdbc.EmbeddedXADataSource;
+
+public class Client {
+
+	String driver = "org.apache.derby.jdbc.EmbeddedDriver";
+	String createMietspiegel = "CREATE TABLE mietspiegel  "
+			+ "(ort VARCHAR(50) NOT NULL,"
+			+ " miete INTEGER NOT NULL) ";
+	String createKindergarten = "CREATE TABLE kindergarten  "
+			+ "(ort VARCHAR(50) NOT NULL,"
+			+ " beitrag INTEGER NOT NULL) ";
+
+	private String dbName;
+	public Connection conn;
+	public EmbeddedXADataSource ds;
+	public XAResource res;
+
+	public Client(String dbName) {
+		this.dbName = dbName;
+	}
+
+	private void createMietspiegel(){
+		try {
+			conn.createStatement().executeUpdate("DROP TABLE mietspiegel");
+			conn.createStatement().executeUpdate(createMietspiegel);
+			conn.createStatement().executeUpdate("insert into mietspiegel (ort,miete) values ('Berlin', 1200)");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void createKindergarten() {
+		try {
+			conn.createStatement().executeUpdate("DROP TABLE kindergarten");
+			conn.createStatement().executeUpdate(createKindergarten);
+			conn.createStatement().executeUpdate("insert into kindergarten (ort,beitrag) values ('Berlin', 100)");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	private void connect() {
+		try {
+			ds = new EmbeddedXADataSource();
+			ds.setCreateDatabase("create");
+			ds.setDatabaseName(dbName);
+			res = ds.getXAConnection().getXAResource();
+			conn = ds.getXAConnection().getConnection();
+			if(dbName.equals("mietspiegel")){
+				createMietspiegel();
+			}else{
+				createKindergarten();
+			}
+			conn.commit();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) throws XAException, SQLException {
+		Client c1 = new Client("mietspiegel");
+		c1.connect();
+		Client c2 = new Client("kindergarten");
+		c2.connect();
+		
+		MyXid xid1 = new MyXid(new byte[]{0x11}, 1, new byte[]{0x12});
+		MyXid xid2 = new MyXid(new byte[]{0x21}, 2, new byte[]{0x22});
+		
+		c1.res.start(xid1, 0);
+		c2.res.start(xid2, 0);
+		int i1 = c1.conn.createStatement().executeUpdate("insert into mietspiegel (ort,miete) values ('Wallenfels', 300)");
+		int i2 = c2.conn.createStatement().executeUpdate("insert into kindergarten (ort,beitrag) values ('Wallenfels', 20)");
+		c1.res.end(xid1, i1 > 0 ? XAResource.TMSUCCESS : XAResource.TMFAIL);
+		c2.res.end(xid2, i2 > 0 ? XAResource.TMSUCCESS : XAResource.TMFAIL);
+		try{
+			System.out.println("prepare 1");
+			int r1 = c1.res.prepare(xid1);
+			System.out.println("prepare 2");
+			int r2 = c2.res.prepare(xid2);
+			if(r1 == XAResource.XA_OK && r2 == XAResource.XA_OK){
+				System.out.println("commit both");
+				c1.res.commit(xid1, false);
+				c2.res.commit(xid2, false);
+			}
+		}catch (XAException e) {
+			System.out.println("rollback both");
+			c1.res.rollback(xid1);
+			c2.res.rollback(xid2);
+		}
+	}
+	
+	static class MyXid implements Xid {
+
+		private byte[] branch;
+		private int format;
+		private byte[] trans;
+
+		public MyXid(byte[] branch, int format, byte[] trans){
+			this.branch = branch;
+			this.format = format;
+			this.trans = trans;
+		}
+		
+		@Override
+		public byte[] getBranchQualifier() {
+			return branch;
+		}
+
+		@Override
+		public int getFormatId() {
+			return format;
+		}
+
+		@Override
+		public byte[] getGlobalTransactionId() {
+			return trans;
+		}
+		
+	}
+
+}
